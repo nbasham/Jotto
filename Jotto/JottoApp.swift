5b2440accbca0f3eeef4c38254d70b3350a0497c
Norman Basham <2135673+nbasham@users.noreply.github.com>
2023-07-16 11:53:49 -0700

    Initial Commit

---
+++ Jotto/JottoApp.swift
@@ -0,0 +1,17 @@
+//
+//  JottoApp.swift
+//  Jotto
+//
+//  Created by Norman Basham on 7/16/23.
+//
+
+import SwiftUI
+
+@main
+struct JottoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
